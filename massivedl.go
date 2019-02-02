44a0c10f2caaccb05a65637570e6a0c5383f0a7b
dimkouv <dimkouv@protonmail.com>
2019-02-02 21:43:48 +0200

    add command line params

--- massivedl.go
+++ massivedl.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -27,13 +28,20 @@ type dataEntry struct {
 	url  string
 }
 
+type cmdLineParams struct {
+	batchSize       int
+	entriesFilepath string
+	skippedLines    int
+	outputDir       string
+}
+
 // loads data entries from a csv file.
 // csv file entries be (output name, url)
 // check examples/ for example .csv files
 // @param filename - The full path of the .csv file to load
-// @param ignoredLines - Number of lines to skip from the beginning
+// @param skippedLines - Number of lines to skip from the beginning
 //                       of the csv file
-func readData(filename string, ignoredLines int) []dataEntry {
+func readData(filename string, skippedLines int) []dataEntry {
 	var entries []dataEntry
 
 	file, err := os.Open(filename)
@@ -44,8 +52,8 @@ func readData(filename string, ignoredLines int) []dataEntry {
 
 	scanner := bufio.NewScanner(file)
 
-	/* pass the ignored lines */
-	for i := 0; i < ignoredLines; i++ {
+	/* pass the skipped lines */
+	for i := 0; i < skippedLines; i++ {
 		scanner.Scan()
 	}
 
@@ -96,14 +104,84 @@ func download(url, filepath string) logEntry {
 	return logRow
 }
 
+func printUsage() {
+	usage := [...]string{
+		"NAME",
+		"\tmassivedl - Download a list of files in parallel batches",
+		"\nSYNOPSIS",
+		"\tmassivedl [OPTION]...",
+		"\nDESCRIPTION",
+		"\tmassivedl is a free utility for non-interactive download of files from the web.",
+		"\tThis utility can be used to download a large list of files from the web in parallel batches.",
+		"\tYou can get really good results when the server you're downloading from has low response time.",
+		"\nOPTIONS",
+		"\t-b <int> ::: Size of a batch ::: default 10",
+		"\t-i <str> ::: Input csv file with the list of urls",
+		"\t-s <int> ::: Number of skipped lines from input csv ::: default 0",
+		"\t-o <str> ::: Directory to place the downloads ::: default 'downloads'",
+		"\nEXAMPLE",
+		"\tmassivedl -b 10 -i data.csv -s 1 -o downloads",
+		"\nAUTHOR",
+		"\tdimkouv <dimkouv@protonmail.com>",
+		"\tContributions at: https://github.com/dimkouv/massivedl",
+		"\n",
+	}
+	fmt.Println(strings.Join(usage[:], "\n"))
+}
+
+func parseCmdLineParams() cmdLineParams {
+	p := cmdLineParams{10, "", 0, "downloads"}
+	var err error
+
+	for i := 0; i < len(os.Args)-1; i++ {
+		if strings.Compare(os.Args[i], "-b") == 0 {
+			// -b ::: size of a batch
+			p.batchSize, err = strconv.Atoi(os.Args[i+1])
+
+			if err != nil {
+				printUsage()
+				log.Fatal("Error parsing command line parameters")
+			}
+		} else if strings.Compare(os.Args[i], "-i") == 0 {
+			// -i ::: entries file path
+			p.entriesFilepath = os.Args[i+1]
+		} else if strings.Compare(os.Args[i], "-s") == 0 {
+			// -s ::: number of skipped lines
+			p.skippedLines, err = strconv.Atoi(os.Args[i+1])
+
+			if err != nil {
+				printUsage()
+				log.Fatal("Error parsing command line parameters")
+			}
+		} else if strings.Compare(os.Args[i], "-o") == 0 {
+			// -o ::: output - downloads directory
+			p.outputDir = os.Args[i+1]
+		}
+	}
+
+	if strings.Compare(p.entriesFilepath, "") == 0 {
+		printUsage()
+		log.Fatal("You have to provide input csv file using -i cmd line param.")
+	}
+
+	return p
+}
+
 func main() {
-	// @TODO replace with cmd line params
-	b := 8                        // number of downloads per batch
-	entriesFilepath := "data.csv" // path of input urls
-	ignoredLines := 1             // skipped lines of input csv file
-	outputDir := "downloads"      // output directory
+	p := parseCmdLineParams()
 
 	totalDownloaded := 0
+
+	// load urls - entries to download
+	entries := readData(p.entriesFilepath, p.skippedLines)
+	n := len(entries)
+
+	// create downloads dir if it doesn't exist
+	os.MkdirAll(p.outputDir, os.ModePerm)
+
+	b := p.batchSize
+
+	fmt.Printf("massivedl about to download %d files\n", n)
 
 	// create log file
 	f, err := os.OpenFile("massivedl.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -114,13 +192,6 @@ func main() {
 
 	// redirect logger output on the log file
 	log.SetOutput(f)
-
-	// load urls - entries to download
-	entries := readData(entriesFilepath, ignoredLines)
-	n := len(entries)
-
-	// create downloads dir if it doesn't exist
-	os.MkdirAll(outputDir, os.ModePerm)
 
 	for i := 0; i < n; {
 		// fix batch size for the last iteration
@@ -141,7 +212,7 @@ func main() {
 		/* call download function for this batch */
 		for j := 0; j < b; j++ {
 			go func(idx int) {
-				logEntries <- download(entries[i].url, path.Join(outputDir, entries[idx].name))
+				logEntries <- download(entries[i].url, path.Join(p.outputDir, entries[idx].name))
 				wg.Done()
 				return
 			}(i + j)
